c9beb47164315f7d8d668d632b5d108e4081674c
MinThetN <minthetn15@gmail.com>
2024-12-23 01:05:17 +0700

    created: about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-4xl font-medium text-amber-600'> About</h1>
+        <p className='text-xl'>IMDb is the world's most popular and authoritative source for information 
+            on movies, TV shows, and celebrities. Products and services to help fans 
+            discover and decide what to watch and where to watch it include: the IMDb 
+            website for desktop and mobile devices; apps for iOS and Android; and, X-Ray 
+            on Prime Video. For entertainment industry professionals, IMDb provides 
+            IMDbPro and Box Office Mojo. IMDb licenses information from its vast and 
+            authoritative database to third-party businesses worldwide; learn more at 
+            developer.imdb.com. IMDb is an Amazon company. For more information, visit 
+            https://www.imdb.com/press and follow @IMDb.</p>
+    </div>
+  )
+}
