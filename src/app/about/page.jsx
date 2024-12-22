01c38c6f4286f7d8c1ee4d39e316b925f09407f5
MinThetN <minthetn15@gmail.com>
2024-12-23 01:17:24 +0700

    updated: colors

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div className='max-w-6xl mx-auto p-3 space-y-4'>
-        <h1 className='text-4xl font-medium text-amber-600'> About</h1>
+        <h1 className='text-4xl font-medium text-lime-500'> About</h1>
         <p className='text-xl'>IMDb is the world's most popular and authoritative source for information 
             on movies, TV shows, and celebrities. Products and services to help fans 
             discover and decide what to watch and where to watch it include: the IMDb 
--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -11,9 +11,9 @@ export default function DarkModeSwitch() {
   return (
     <div>
         {currentTheme === 'dark' ? <MdLightMode onClick={()=>setTheme('light')}
-        className='text-xl cursor-pointer hover:text-amber-500'/> : 
+        className='text-xl cursor-pointer hover:text-lime-500'/> : 
         <MdDarkMode onClick={()=>setTheme('dark')}
-        className='text-xl cursor-pointer hover:text-amber-500'/>}
+        className='text-xl cursor-pointer hover:text-lime-500'/>}
     </div>
   )
 }
