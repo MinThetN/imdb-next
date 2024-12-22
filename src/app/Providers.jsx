45fc06eb3c3be3219d4949b7c43062f689414678
MinThetN <minthetn15@gmail.com>
2024-12-23 00:46:54 +0700

    updated: transtion

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -5,7 +5,8 @@ import { ThemeProvider } from 'next-themes'
 export default function Providers({children}) {
   return (
     <ThemeProvider defaultTheme='system' attribute='class'>
-        <div className=''>
+        <div className='text-gray-700 dark:text-gray-200 dark:bg-gray-800
+        min-h-screen selection:none transition-colors duration-500'>
             {children}
         </div>
     </ThemeProvider>
